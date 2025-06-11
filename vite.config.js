fcb17ba936b3e1d1fb8dbf3f01907bba5e46407f
Ehsan ... <ghannad73@gmail.com>
2025-06-11 16:26:40 +0330

    add milion linter

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
+import MillionLint from "@million/lint";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [MillionLint.vite({
+    enabled: true
+  }), react()],
 });
